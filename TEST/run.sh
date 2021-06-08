45ec751d8c72314dc3ea9ad5791d1479da311a08
senselogic <dev.senselogic@gmail.com>
2021-06-08 20:56:31 +0100

    % test

--- TEST/run.sh
+++ TEST/run.sh
@@ -8,3 +8,5 @@ set -x
 ../BUILD/xenius --read-e57-cloud IN/cube_3.e57 --write-xyz-cloud OUT/cube_3.xyz
 ../BUILD/xenius --read-e57-cloud IN/teapot.e57 --write-xyz-cloud OUT/teapot.xyz
 ../BUILD/xenius --read-e57-cloud IN/bunny.e57 --write-xyz-cloud OUT/bunny.xyz
+../BUILD/xenius --read-e57-cloud IN/bunny.e57 --write-pts-cloud OUT/bunny.pts XYZIRGB
+../BUILD/xenius --position-rotation 0 0 -45 --read-e57-cloud IN/bunny.e57 --write-xyz-cloud OUT/rotated_bunny.xyz
